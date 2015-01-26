1be01502d816e4e64ac08293bc98552677951ad5
Deyan Dobromirov <dvd_video@abv.bg>
2015-01-26 17:32:15 +0200

    Added: Player jump power.

--- lua/weapons/gmod_tool/stools/jediforce.lua
+++ lua/weapons/gmod_tool/stools/jediforce.lua
@@ -10,6 +10,7 @@ TOOL.ClientConVar =
   [ "movemap" ]           = 0,
   [ "axislen" ]           = 30,
   [ "enablecs" ]          = 1,
+  [ "jumpower" ]          = 200,
   [ "storeposkey" ]       = "",
   [ "forceamount" ]       = 500,
   [ "forcemcapply" ]      = 0,
@@ -28,27 +29,30 @@ local string = string
 local RunConsoleCommand = RunConsoleCommand
 local LocalPlayer = LocalPlayer
 
-local SavedLocations = {}
+local Saved = {
+  JumpPower = -1,
+  Locations = {}
+}
 
-function ApplyJediForce(oSelf,stTrace,vVec)
+function ApplyJediForce(oTool,stTrace,vVec)
   local oEnt = stTrace.Entity
   if(oEnt and oEnt:IsValid()) then
     local oPhy = oEnt:GetPhysicsObject()
     if(oPhy and oPhy:IsValid()) then
       -- A jedi cant pull the map ...
-      local force       = oSelf:GetClientNumber( "forceamount", 0 )
-      local maxdistance = oSelf:GetClientNumber( "forcemaxdistance", 0 )
-      local massrelative= oSelf:GetClientNumber( "forcemassrelative", 0 )
-      local distrelative= oSelf:GetClientNumber( "forcedistrelative", 0 )
-      local mcapply     = oSelf:GetClientNumber( "forcemcapply", 0 )
+      local force       = oTool:GetClientNumber( "forceamount", 0 )
+      local maxdistance = oTool:GetClientNumber( "forcemaxdistance", 0 )
+      local massrelative= oTool:GetClientNumber( "forcemassrelative", 0 )
+      local distrelative= oTool:GetClientNumber( "forcedistrelative", 0 )
+      local mcapply     = oTool:GetClientNumber( "forcemcapply", 0 )
       local vF          = force*vVec:GetNormalized()
-      if(massrelative ~= 0) then
+      if(massrelative and massrelative ~= 0) then
         vF:Mul(oPhy:GetMass())
       end
-      if(distrelative ~= 0) then
+      if(distrelative and maxdistance and distrelative ~= 0) then
         vF:Mul(math.Clamp((1-vVec:Length()/maxdistance),0,1))
       end
-      if(maxdistance > vVec:Length()) then
+      if(maxdistance and maxdistance > vVec:Length()) then
         if(mcapply ~= 0) then
           oPhy:ApplyForceCenter(vF)
         else
@@ -66,6 +70,7 @@ if CLIENT then
   language.Add("tool.jediforce.force", "Force Amount")
   language.Add("tool.jediforce.distance", "Force Max Distance")
   language.Add("tool.jediforce.axislen", "Vision Axis Length")
+  language.Add("tool.jediforce.jumpower", "Jedi Jump power")
 end
 
 function TOOL:Think()
@@ -90,10 +95,10 @@ function TOOL:DrawHUD()
   local oPly    = self:GetOwner()
   local stTrace = oPly:GetEyeTrace()
   if(stTrace and oPly) then
-    local oEnt    = stTrace.Entity
-    local axislen = self:GetClientNumber( "axislen",  30 )
     local enucs   = self:GetClientNumber( "enablecs", 0 )
     if(enucs ~= 0) then
+      local oEnt    = stTrace.Entity
+      local axislen = self:GetClientNumber( "axislen",  30 )
       if(oEnt and oEnt:IsValid() and not oEnt:IsWorld()) then
         local vPos = oEnt:GetPos()
         local nRad = (vPos - oPly:GetPos()):Length()
@@ -101,9 +106,9 @@ function TOOL:DrawHUD()
         local O = vPos:ToScreen()
         local B = (oEnt:LocalToWorld(oEnt:OBBCenter())):ToScreen()
         local M = (oEnt:GetNWVector("jediforce_MC_vector")):ToScreen()
-        local X = (vPos + axislen*oEnt:GetForward()):ToScreen()
-        local Y = (vPos - axislen*oEnt:GetRight()):ToScreen()
-        local Z = (vPos + axislen*oEnt:GetUp()):ToScreen()
+        local X = (vPos + axislen * oEnt:GetForward()):ToScreen()
+        local Y = (vPos - axislen * oEnt:GetRight()):ToScreen()
+        local Z = (vPos + axislen * oEnt:GetUp()):ToScreen()
         surface.SetDrawColor(255,0,0,255)
         surface.DrawLine( O.x, O.y, X.x, X.y )
         surface.SetDrawColor(0,255,0,255)
@@ -148,50 +153,63 @@ end
 function TOOL:Reload( Trace )
   if(CLIENT) then return end
   if(not Trace) then return end
-  local oPly = self:GetOwner()
-  local oEnt = Trace.Entity
-  if(oEnt and oEnt:IsValid()) then
+  local ply = self:GetOwner()
+  local trEnt = Trace.Entity
+  if(trEnt and trEnt:IsValid()) then
     local movemap = self:GetClientNumber("movemap") or 0
-    local class = oEnt:GetClass()
+    local class = trEnt:GetClass()
     if(class ~= "prop_physics" and movemap == 0) then return end
-    if(oEnt:GetPhysicsObject():IsValid()) then
+    if(trEnt:GetPhysicsObject():IsValid()) then
       -- A jedi cant grab the map ...
-      local vAim = oPly:GetAimVector()
+      local vAim = ply:GetAimVector()
       -- Dont grab over you
       vAim.z = 0
       -- Place the prop in radius of 55 gmu
       if(vAim:Length() < 55) then
-        vAim:Mul(55/vAim:Length())
+        vAim:Mul(55 / vAim:Length())
       end
-      oEnt:SetPos(oPly:GetPos()+vAim+Vector(0,0,45))
+      trEnt:SetPos(ply:GetPos() + vAim + Vector(0,0,45))
     end
   elseif(Trace.HitWorld) then
-    local spd = oPly:KeyDown(IN_SPEED) 
-    local use = oPly:KeyDown(IN_USE)
+    local use = ply:KeyDown(IN_USE)
+    local duc = ply:KeyDown(IN_DUCK)
+    local spd = ply:KeyDown(IN_SPEED) 
     local storeposkey = self:GetClientInfo("storeposkey") or ""
     if(use and string.len(storeposkey) > 0) then
-      local plPos = oPly:GetPos()
-      SavedLocations[storeposkey] = { plPos[cvX], plPos[cvY], plPos[cvZ]}
+      local plPos = ply:GetPos()
+      Saved.Locations[storeposkey] = { plPos[cvX], plPos[cvY], plPos[cvZ]}
       return true
-    elseif(spd and string.len(storeposkey) > 0 and SavedLocations[storeposkey]) then
-      local loc = SavedLocations[storeposkey]
+    elseif(spd and string.len(storeposkey) > 0 and Saved.Locations[storeposkey]) then
+      local loc = Saved.Locations[storeposkey]
       local x = loc[cvX]
       local y = loc[cvY]
       local z = loc[cvZ]
       if(x and y and z) then
-        oPly:SetVelocity(-oPly:GetVelocity())
-        oPly:SetPos(Vector(x,y,z))
+        ply:SetVelocity(-ply:GetVelocity())
+        ply:SetPos(Vector(x,y,z))
         return true
       end
       return false
-    elseif(not use and not spd) then
-      oPly:SetVelocity(-oPly:GetVelocity())
-      oPly:SetPos(Trace.HitPos + 20 * Trace.HitNormal)
+    elseif(duc) then
+      if(Saved.JumpPower == -1) then
+        Saved.JumpPower = ply:GetJumpPower()
+      end
+      jumpower = math.Clamp(self:GetClientNumber("jumpower"),0,100000)
+      ply:SetJumpPower(jumpower)
+    elseif(not use and not spd and not duc) then
+      ply:SetVelocity(-ply:GetVelocity())
+      ply:SetPos(Trace.HitPos + 20 * Trace.HitNormal)
       return true
     end
     return true
   end
   return false
+end
+
+function TOOL:Holster()
+  local ply = self:GetOwner()
+  if(not ply) then return false end
+  ply:GetJumpPower(Saved.JumpPower)
 end
 
 function TOOL.BuildCPanel( cPanel )
@@ -214,7 +232,8 @@ function TOOL.BuildCPanel( cPanel )
       [4] = "jediforce_forcemcapply",
       [5] = "jediforce_enablecs",
       [6] = "jediforce_axislen",
-      [7] = "jediforce_movemap"
+      [7] = "jediforce_movemap",
+      [8] = "jediforce_jumpower"
     }
   })
   
@@ -236,11 +255,11 @@ function TOOL.BuildCPanel( cPanel )
           local psKey = self:GetValue() or ""
           local plPos = LocalPlayer():GetPos()
           if(string.len(psKey) > 0) then
-            if(not SavedLocations[psKey]) then
+            if(not Saved.Locations[psKey]) then
               pTele:AddChoice(psKey)
             end
             RunConsoleCommand("jediforce_storeposkey",psKey)
-            SavedLocations[psKey] = {plPos[cvX], plPos[cvY], plPos[cvZ]}
+            Saved.Locations[psKey] = {plPos[cvX], plPos[cvY], plPos[cvZ]}
           end
         end
   cPanel:AddItem(pText)
@@ -269,6 +288,14 @@ function TOOL.BuildCPanel( cPanel )
       Max     = 500,
       Command = "jediforce_axislen",
       Description = "Sets the UCS axis length"})
+      
+  cPanel:AddControl( "Slider",  {
+      Label   = "#tool.jediforce.jumpower",
+      Type    = "Float",
+      Min     = 0,
+      Max     = 100000,
+      Command = "jediforce_jumpower",
+      Description = "Sets the UCS axis length"})  
       
   cPanel:AddControl("CheckBox",{
       Label       = "Force multiplyed by prop mass",
