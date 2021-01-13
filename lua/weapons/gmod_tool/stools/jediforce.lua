1a1fc14f133a32f5cec70aab6b0efadb7acf4e72
Deyan Dobromirov <dvd_video@abv.bg>
2021-01-13 18:48:49 +0200

    Fixed: Distance relative default value is zero Fixed: Default jump power is 200 Fixed: Stored location are properly added/deleted via net messages Fixed: Holster properly defaults jump power Added: Dedicated convar reader tool methods Added: Force grows with distance option Added: Language translation hashes Optimized: Left-right click and draw HUD routines Removed: Location stored via R+E as not needed anymore. Handel by net.* Removed: Convar enablecs. it can be achieved by setting axis to zero Bind: Jump power change to R+E

--- lua/weapons/gmod_tool/stools/jediforce.lua
+++ lua/weapons/gmod_tool/stools/jediforce.lua
@@ -10,7 +10,7 @@ TOOL.ClientConVar =
   [ "mcapply"      ] = 0  ,
   [ "enablecs"     ] = 1  ,
   [ "massrelative" ] = 0  ,
-  [ "distrelative" ] = 1  ,
+  [ "distreverse"  ] = 0  ,
   [ "axislen"      ] = 30 ,
   [ "jumpower"     ] = 200,
   [ "storeposkey"  ] = "" ,
@@ -34,75 +34,131 @@ local LocalPlayer       = LocalPlayer
 local GetConVarString   = GetConVarString
 local RunConsoleCommand = RunConsoleCommand
 
-function GetJediInfo(oPly)
+function GetJediInfo(oPly, bDel)
   if(not oPly) then return nil end
   if(not oPly:IsValid()) then return nil end
   if(not oPly:IsPlayer()) then return nil end
+  if(bDel) then gtSaved[oPly] = nil; return end
   local tPly = gtSaved[oPly]
   if(not tPly) then
     gtSaved[oPly] = {
-      JumpPow = -1,
       MovePos = {},
-      JumpDef = oPly:GetJumpPower()
+      JumpDef = 200
     }
     tPly = gtSaved[oPly]
   end; return tPly, oPly
 end
 
-function TOOL:ApplyJediForce(stTrace, vVec)
-  local oEnt = stTrace.Entity
+function TOOL:GetDistReverse()
+  return ((tonumber(self:GetClientNumber("distreverse", 0)) or 0) ~= 0)
+end
+
+function TOOL:GetMassRelative()
+  return ((tonumber(self:GetClientNumber("massrelative", 0)) or 0) ~= 0)
+end
+
+function TOOL:GetForceAtMC()
+  return ((tonumber(self:GetClientNumber("mcapply", 0)) or 0) ~= 0)
+end
+
+function TOOL:GetMoveMap()
+  return ((tonumber(self:GetClientNumber("movemap", 0)) or 0) ~= 0)
+end
+
+function TOOL:GetForce()
+  return math.Clamp((tonumber(self:GetClientNumber("force", 0)) or 0), 0, 100000)
+end
+
+function TOOL:GetDistance()
+  return math.Clamp((tonumber(self:GetClientNumber("distance", 0)) or 0), 0, 100000)
+end
+
+function TOOL:GetJumpAmount()
+  return math.Clamp((tonumber(self:GetClientNumber("jumpower", 0)) or 0), 0, 10000)
+end
+
+function TOOL:GetAxisSize()
+  return math.Clamp((tonumber(self:GetClientNumber("axislen", 0)) or 0), 0, 500)
+end
+
+function TOOL:ApplyJediForce(stTr, vVec)
+  local oEnt = stTr.Entity
   if(oEnt and oEnt:IsValid()) then
     local oPhy = oEnt:GetPhysicsObject()
     if(oPhy and oPhy:IsValid()) then -- A jedi cant pull the map ...
-      local force        = self:GetClientNumber("force", 0)
-      local mcapply      = self:GetClientNumber("mcapply", 0)
-      local distance     = self:GetClientNumber("distance", 0)
-      local massrelative = self:GetClientNumber("massrelative", 0)
-      local distrelative = self:GetClientNumber("distrelative", 0)
+      local nV           = vVec:Length()
+      local force        = self:GetForce()
+      local distance     = self:GetDistance()
+      local mcapply      = self:GetForceAtMC()
+      local massrelative = self:GetMassRelative()
+      local distreverse  = self:GetDistReverse()
       local vF           = force * vVec:GetNormalized()
-      if(massrelative and massrelative ~= 0) then
-        vF:Mul(oPhy:GetMass())
-      end
-      if(distrelative and distance and distrelative ~= 0) then
-        vF:Mul(math.Clamp((1 - vVec:Length() / distance), 0, 1))
-      end
-      if(distance and distance > vVec:Length()) then
-        if(mcapply ~= 0) then
-          oPhy:ApplyForceCenter(vF)
-        else
-          oPhy:ApplyForceOffset(vF, stTrace.HitPos)
+      if(massrelative) then vF:Mul(oPhy:GetMass()) end
+      if(distance > 0) then
+        if(distreverse) then -- Bigger force
+          vF:Mul(math.Clamp(nV / distance, 0, 1))
+        else -- Smaller force on distant objects
+          vF:Mul(math.Clamp(1 - (nV / distance), 0, 1))
         end
+      end
+      if(mcapply) then
+        oPhy:ApplyForceCenter(vF)
+      else
+        oPhy:ApplyForceOffset(vF, stTr.HitPos)
       end
     end
   end
 end
 
-if CLIENT then
+if(CLIENT) then
   language.Add("tool."..gsToolName..".name", "Jedi Force Tool")
   language.Add("tool."..gsToolName..".desc", "Uses jedi's force to pull/push object")
   language.Add("tool."..gsToolName..".0", "Left click to Push, Right click to Pull, Reload to Grab")
-  language.Add("tool."..gsToolName..".force", "Force amount")
-  language.Add("tool."..gsToolName..".force_con", "Sets the amount of force to be used")
-  language.Add("tool."..gsToolName..".distance", "Change here to adjust the dorce distance")
+  language.Add("tool."..gsToolName..".force", "Sets the amount of force to be used")
+  language.Add("tool."..gsToolName..".force_con", "Force amount")
+  language.Add("tool."..gsToolName..".distance", "Adjusts the relative force dropping with distance. Write zero to disable")
   language.Add("tool."..gsToolName..".distance_con", "Force distance")
-  language.Add("tool."..gsToolName..".axislen", "Sets the UCS axis length")
-  language.Add("tool."..gsToolName..".axislen_con", "Vision axis length")
+  language.Add("tool."..gsToolName..".axislen", "Change this to adjust UCS axis length. Write zero to disable")
+  language.Add("tool."..gsToolName..".axislen_con", "Axis length")
   language.Add("tool."..gsToolName..".jumpower", "Change this to adjust the jump power")
-  language.Add("tool."..gsToolName..".jumpower_con", "Jedi Jump power")
+  language.Add("tool."..gsToolName..".jumpower_con", "Jedi jump power")
   language.Add("tool."..gsToolName..".massrelative", "Enable to multiply force by the entity mass")
   language.Add("tool."..gsToolName..".massrelative_con", "Force multiplyed by mass")
-  language.Add("tool."..gsToolName..".distrelative", "Enable to make the force get lower on distant objects")
-  language.Add("tool."..gsToolName..".distrelative_con", "Force dropping with distance")
+  language.Add("tool."..gsToolName..".distreverse", "Enable to make the force get higher on distant objects")
+  language.Add("tool."..gsToolName..".distreverse_con", "Force growing with distance")
   language.Add("tool."..gsToolName..".mcapply", "Enable to make the force applied at the entity masscentre")
   language.Add("tool."..gsToolName..".mcapply_con", "Apply force at the masscentre")
-  language.Add("tool."..gsToolName..".enablecs", "Enable this to view entity or world coordinate system")
-  language.Add("tool."..gsToolName..".enablecs_con", "Enable Jedi's UCS Vision")
   language.Add("tool."..gsToolName..".movemap", "Enable this to allow moving map props with mind power")
   language.Add("tool."..gsToolName..".movemap_con", "Enable moving map props")
-  language.Add("tool."..gsToolName..".storeposkey_ls", "Different recall locations are stored here")
-  language.Add("tool."..gsToolName..".storeposkey_ls_con", "MovePos memory")
+  language.Add("tool."..gsToolName..".storeposkey_ls", "Different recall location names are stored here")
+  language.Add("tool."..gsToolName..".storeposkey_ls_def", "<CHOSE LOCATION>")
+  language.Add("tool."..gsToolName..".storeposkey_ls_con", "Location memory")
   language.Add("tool."..gsToolName..".storeposkey_tx", "Write down location name and press ENTER to store it")
-  language.Add("tool."..gsToolName..".storeposkey_tx_con", "Locationn name")
+  language.Add("tool."..gsToolName..".storeposkey_tx_con", "Location name")
+elseif(SERVER) then
+  hook.Add("PlayerDisconnected", gsToolName.."_player_quit",
+    function(oPly) GetJediInfo(oPly, true) end)
+
+  util.AddNetworkString(gsToolName.."_storeposkey")
+  net.Receive(gsToolName.."_storeposkey", function(nLen)
+    local oPly = net.ReadEntity()
+    if(not (oPly and oPly:IsValid())) then return end
+    local vPos = net.ReadVector()
+    local sKey = net.ReadString()
+    if(sKey:len() <= 0) then return end
+    local tPly = GetJediInfo(oPly)
+          tPly.MovePos[sKey] = vPos
+  end)
+
+  util.AddNetworkString(gsToolName.."_deleteposkey")
+  net.Receive(gsToolName.."_deleteposkey", function(nLen)
+    local oPly = net.ReadEntity()
+    if(not (oPly and oPly:IsValid())) then return end
+    local sKey = net.ReadString()
+    if(sKey:len() <= 0) then return end
+    local tPly = GetJediInfo(oPly)
+          tPly.MovePos[sKey] = nil
+  end)
 end
 
 function TOOL:Think()
@@ -120,19 +176,22 @@ function TOOL:Think()
   end
 end
 
+function TOOL:GetPosRadius(oPly, vHit, nAxs)
+  local nRad = (vHit - oPly:GetPos()):Length()
+  return math.Clamp(20 * nAxs / nRad, 1, 100)
+end
+
 function TOOL:DrawHUD()
   if(SERVER) then return end
   local oPly = self:GetOwner()
   local stTr = oPly:GetEyeTrace()
   if(stTr and oPly) then
-    local enucs   = self:GetClientNumber("enablecs", 0)
-    if(enucs ~= 0) then
-      local oEnt    = stTr.Entity
-      local axislen = self:GetClientNumber("axislen", 30)
+    local axislen = self:GetAxisSize()
+    if(axislen > 0) then
+      local oEnt = stTr.Entity
       if(oEnt and oEnt:IsValid() and not oEnt:IsWorld()) then
         local vPos = oEnt:GetPos()
-        local nRad = (vPos - oPly:GetPos()):Length()
-              nRad = math.Clamp(20*axislen/nRad,1,100)
+        local nRad = self:GetPosRadius(oPly, vPos, axislen)
         local O = vPos:ToScreen()
         local B = (oEnt:LocalToWorld(oEnt:OBBCenter())):ToScreen()
         local M = (oEnt:GetNWVector(gsToolName.."_MC")):ToScreen()
@@ -150,8 +209,7 @@ function TOOL:DrawHUD()
         surface.DrawCircle( M.x, M.y, nRad, Color(255,255,0,255) )
       elseif(oEnt:IsWorld()) then
         local vPos = stTr.HitPos
-        local nRad = (vPos - oPly:GetPos()):Length()
-              nRad = math.Clamp(20*axislen/nRad,1,100)
+        local nRad = self:GetPosRadius(oPly, vPos, axislen)
         local O = vPos:ToScreen()
         local X = (vPos + axislen*Vector(1,0,0)):ToScreen()
         local Y = (vPos + axislen*Vector(0,1,0)):ToScreen()
@@ -169,14 +227,16 @@ function TOOL:DrawHUD()
 end
 
 function TOOL:LeftClick(tr)
-  if ( CLIENT ) then return end
-  if (not Trace) then return end
+  if(CLIENT) then return end
+  if(not tr) then return end
+  if(not tr.Hit) then return end
   self:ApplyJediForce(tr, tr.HitPos - tr.StartPos)
 end
 
 function TOOL:RightClick(tr)
-  if ( CLIENT ) then return end
-  if (not Trace) then return end
+  if(CLIENT) then return end
+  if(not tr) then return end
+  if(not tr.Hit) then return end
   self:ApplyJediForce(tr, tr.StartPos - tr.HitPos)
 end
 
@@ -186,38 +246,28 @@ function TOOL:Reload(tr)
   local ply = self:GetOwner()
   local trEnt = tr.Entity
   if(trEnt and trEnt:IsValid()) then
-    local movemap = (self:GetClientNumber("movemap") or 0)
-    if(trEnt:GetClass() ~= "prop_physics" and movemap == 0) then return end
-    local vAim = ply:GetAimVector()
-    vAim.z = 0 -- Dont grab over you
-    if(vAim:Length() < 55) then
-      vAim:Mul(55 / vAim:Length())
-    end -- Place the prop in radius of 55 gmu
+    local movemap = self:GetMoveMap()
+    if(not movemap and trEnt:GetClass() ~= "prop_physics") then return end
+    local vAim = ply:GetAimVector(); vAim.z = 0 -- Dont grab over you
+    if(vAim:Length() < 55) then vAim:Mul(55 / vAim:Length()) end
     trEnt:SetPos(ply:GetPos() + vAim + Vector(0, 0, 45))
-    return true
+    return true -- Place the prop in radius of 55 gmu
   elseif(tr.HitWorld) then
-    local tPly = self:GetJediInfo(ply)
+    local tPly = GetJediInfo(ply)
     local use = ply:KeyDown(IN_USE)
-    local duc = ply:KeyDown(IN_DUCK)
     local spd = ply:KeyDown(IN_SPEED)
     local storeposkey = (self:GetClientInfo("storeposkey") or "")
-    if(use and storeposkey:len() > 0) then
-      local plPos = ply:GetPos()
-      tPly.MovePos[storeposkey] = {plPos[cvX], plPos[cvY], plPos[cvZ]}
-      return true
-    elseif(spd and storeposkey:len() > 0 and tPly.MovePos[storeposkey]) then
-      local loc = tPly.MovePos[storeposkey]
-      local x, y, z = loc[cvX], loc[cvY], loc[cvZ]
-      if(x and y and z) then
+    if(spd and storeposkey:len() > 0 and tPly.MovePos[storeposkey]) then
+      local tPos = tPly.MovePos[storeposkey]
+      local nX, nY, nZ = tPos[cvX], tPos[cvY], tPos[cvZ]
+      if(nX and nY and nZ) then
         ply:SetVelocity(-ply:GetVelocity())
-        ply:SetPos(Vector(x, y, z))
+        ply:SetPos(Vector(nX, nY, nZ))
         return true
       end
-      return false
-    elseif(duc) then
-      jumpower = math.Clamp(self:GetClientNumber("jumpower"),0,10000)
-      tPly.JumpPow = jumpower; ply:SetJumpPower(jumpower)
-    elseif(not use and not spd and not duc) then
+    elseif(use) then
+      ply:SetJumpPower(self:GetJumpAmount()); return true
+    elseif(not spd and not use) then
       ply:SetVelocity(-ply:GetVelocity())
       ply:SetPos(tr.HitPos + 20 * tr.HitNormal)
       return true
@@ -228,15 +278,16 @@ function TOOL:Reload(tr)
 end
 
 function TOOL:Holster()
-  local tPly = self:GetJediInfo()
-  if(not tPly) then return false end
-  self:GetOwner():SetJumpPower(tPly.JumpDef)
+  local oPly = self:GetOwner()
+  if(not (oPly and oPly:IsValid())) then return end
+  local tPly = GetJediInfo(oPly)
+  oPly:SetJumpPower(tPly.JumpDef)
 end
 
 -- Enter `spawnmenu_reload` in the console to reload the panel
 local gtConvarList = TOOL:BuildConVarList()
-function TOOL.BuildCPanel(cPanel)
-  cPanel:ClearControls(); cPanel:DockPadding(5, 0, 5, 10); local pItem
+function TOOL.BuildCPanel(cPanel) local pItem, pComb, pText
+  cPanel:ClearControls(); cPanel:DockPadding(5, 0, 5, 10)
   pItem = cPanel:SetName(language.GetPhrase("tool."..gsToolName..".name"))
   pItem = cPanel:Help   (language.GetPhrase("tool."..gsToolName..".desc"))
 
@@ -250,52 +301,76 @@ function TOOL.BuildCPanel(cPanel)
   cPanel:AddItem(pPresets)
 
   -- http://wiki.garrysmod.com/page/Category:DComboBox
-  local pComb = cPanel:ComboBox(language.GetPhrase("tool."..gsToolName..".storeposkey_ls_con"), gsToolName.."_storeposkey")
-               pComb:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_ls"))
-  local storeposkey = (GetConVar(gsToolName.."_storeposkey"):GetString() or "")
-  local pText = cPanel:TextEntry(language.GetPhrase("tool."..gsToolName..".storeposkey_tx_con"))
-                pText:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_tx"))
-
-  -- Combo box modifies text entry and vice-versa
+  pComb, pItem = cPanel:ComboBox(language.GetPhrase("tool."..gsToolName..".storeposkey_ls_con"), gsToolName.."_storeposkey")
+  pComb:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_ls"))
+  pComb:SetValue  (language.GetPhrase("tool."..gsToolName..".storeposkey_ls_def"))
+  pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_ls"))
   pComb:Dock(TOP); pComb:SetTall(20)
-  pComb:SetValue("<Select location NAME>")
-  pComb.OnSelect = function(pnSelf, iD, aVal)
-    RunConsoleCommand(gsToolName.."_storeposkey", aVal)
-  end
 
+  local storeposkey = (GetConVar(gsToolName.."_storeposkey"):GetString() or "")
+  pText, pItem = cPanel:TextEntry(language.GetPhrase("tool."..gsToolName..".storeposkey_tx_con"))
+  pText:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_tx"))
+  pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".storeposkey_tx"))
   pText:Dock(TOP); pText:SetTall(20)
   pText:SetText(storeposkey == "" and "" or storeposkey)
-  pText.OnEnter = function(pnSelf, iD, aVal)
-    local opPly = LocalPlayer()
-    if(opPly and opPly:IsValid()) then
-      local psKey = (pnSelf:GetValue() or "")
-      local vpPos = opPly:GetPos()
-      if(psKey:len() > 0) then
-        local tPly = GetJediInfo(opPly)
-        if(not tPly.MovePos[psKey]) then
-          pComb:AddChoice(psKey)
+
+  pComb.OnSelect = function(pnSelf, iD, aVal)
+    if(input.IsKeyDown(KEY_LSHIFT)) then
+      local oPly = LocalPlayer()
+      if(oPly and oPly:IsValid()) then
+        local tPly = GetJediInfo(oPly)
+        net.Start(gsToolName.."_deleteposkey")
+        net.WriteEntity(oPly)
+        net.WriteString(aVal)
+        net.SendToServer()
+        pnSelf:CloseMenu()
+        table.remove(pnSelf.Data   , iD)
+        table.remove(pnSelf.Choices, iD)
+        tPly.MovePos[aVal] = nil
+        if(not pnSelf.Choices[1]) then
+          pnSelf:Clear(); GetJediInfo(oPly, true) -- Clear the context menu
+          pnSelf:SetValue(language.GetPhrase("tool."..gsToolName..".storeposkey_ls_def"))
         end
-        tPly.MovePos[psKey] = {plPos[cvX], plPos[cvY], plPos[cvZ]}
+      end
+    else
+      RunConsoleCommand(gsToolName.."_storeposkey", aVal)
+    end
+  end
+
+  pText.OnEnter = function(pnSelf, aVal)
+    local oPly = LocalPlayer()
+    if(oPly and oPly:IsValid()) then
+      local vPos = oPly:GetPos()
+      if(aVal:len() > 0) then
+        local tPly = GetJediInfo(oPly)
+        if(not tPly.MovePos[aVal]) then
+          pComb:AddChoice(aVal, aVal)
+        end
+        net.Start(gsToolName.."_storeposkey")
+        net.WriteEntity(oPly)
+        net.WriteVector(vPos)
+        net.WriteString(aVal)
+        net.SendToServer()
+        tPly.MovePos[aVal] = vPos
       end
     end
   end
 
   pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".distance_con"), gsToolName.."_distance", 0, 100000, 3)
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".distance"))
-  pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".force_con"), gsToolName.."_force", 1, 100000, 3)
+  pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".force_con"), gsToolName.."_force", 0, 100000, 3)
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".force"))
-  pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".axislen_con"), gsToolName.."_axislen", 1, 500, 3)
+  pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".axislen_con"), gsToolName.."_axislen", 0, 500, 3)
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".axislen"))
   pItem = cPanel:NumSlider(language.GetPhrase("tool."..gsToolName..".jumpower_con"), gsToolName.."_jumpower", 0, 10000, 3)
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".jumpower"))
+          pItem:SetDefaultValue(gtConvarList[gsToolName.."_jumpower"]) -- Default value for the player jump power
   pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".massrelative_con"), gsToolName.."_massrelative")
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".massrelative"))
-  pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".distrelative_con"), gsToolName.."_distrelative")
-          pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".distrelative"))
+  pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".distreverse_con"), gsToolName.."_distreverse")
+          pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".distreverse"))
   pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".mcapply_con"), gsToolName.."_mcapply")
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".mcapply"))
-  pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".enablecs_con"), gsToolName.."_enablecs")
-          pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".enablecs"))
   pItem = cPanel:CheckBox (language.GetPhrase("tool."..gsToolName..".movemap_con"), gsToolName.."_movemap")
           pItem:SetTooltip(language.GetPhrase("tool."..gsToolName..".movemap"))
 end
