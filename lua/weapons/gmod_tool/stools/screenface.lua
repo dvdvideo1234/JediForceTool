c2372e538c6c6165a7c3a032166b4d13a1b10d1a
Deyan Dobromirov <dvd_video@abv.bg>
2021-10-12 12:48:45 +0300

    Fixed: Screen-face failing at adjusting Z pos

--- lua/weapons/gmod_tool/stools/screenface.lua
+++ lua/weapons/gmod_tool/stools/screenface.lua
@@ -18,6 +18,20 @@ if(CLIENT) then
   language.Add("tool."..gsTool..".0", "Left click to face the player, right to pick angle, right+shift to pick location")
 end
 
+local NTIF = {
+  "GAMEMODE:AddNotify(\"%s\", NOTIFY_%s, 6)",
+  "surface.PlaySound(\"ambient/water/drip%d.wav\")"
+}
+
+local function Notify(user, text, mtyp)
+  if(LaserLib.IsValid(user)) then
+    if(SERVER) then local ran = math.random(1, 4)
+      user:SendLua(NTIF[1]:format(text, mtyp))
+      user:SendLua(NTIF[2]:format(ran))
+    end
+  end
+end
+
 local function GetOffsetUP(ent, dir)
   if(not (ent and ent:IsValid())) then return end
   local out = ent:OBBCenter()
@@ -25,7 +39,7 @@ local function GetOffsetUP(ent, dir)
   return math.abs(obb:Dot(dir) / 2)
 end
 
-local function SetFacePlayer(ply, pos, ent, nrm, ang)
+local function SetFacePlayer(ply, ent, nrm, pos, ang)
   if(not (ent and ent:IsValid())) then return end
   local norm, epos = Vector(nrm), ent:GetPos()
         norm:Normalize() -- Make sure it is normalized
@@ -35,9 +49,10 @@ local function SetFacePlayer(ply, pos, ent, nrm, ang)
   local tang = ent:AlignAngles(ent:LocalToWorldAngles(cang), rang)
         tang:Normalize(); tang:RotateAroundAxis(norm, 180)
   ent:SetAngles(tang) -- Apply the angle as long as it is ready
-  local vobb = ent:OBBCenter(); vobb:Rotate(tang)
-        vobb.x, vobb.y = -vobb.x, -vobb.y -- Revert OBB to position
-        vobb.z = GetOffsetUP(ent, ent:WorldToLocal(norm + epos))
+  local vobb = ent:OBBCenter() -- Revert OBB to position
+        vobb.x, vobb.y, vobb.z = -vobb.x, -vobb.y, -vobb.z
+  local marg = GetOffsetUP(ent, ent:WorldToLocal(norm + epos))
+        vobb:Rotate(tang); vobb:Add(norm * marg)
   local tpos = Vector(vobb); tpos:Add(pos) -- Use OBB offset
   ent:SetPos(tpos) -- Apply the calculated position
 end
@@ -48,7 +63,7 @@ function TOOL:LeftClick(tr)
   if(not tr.Hit) then return end
   local vpos = Vector(self:GetClientInfo("position"))
   local vang = Angle(self:GetClientInfo("angle"))
-  SetFacePlayer(self:GetOwner(), vpos, tr.Entity, Vector(0,0,1), vang)
+  SetFacePlayer(self:GetOwner(), tr.Entity, Vector(0,0,1), vpos, vang)
   return true
 end
 
@@ -58,14 +73,24 @@ function TOOL:RightClick(tr)
   if(not tr.Hit) then return end
   local ply = self:GetOwner()
   if(ply:KeyDown(IN_SPEED)) then
-    print("Position: "..tostring(tr.HitPos))
-    ply:ConCommand(gsTool.."_position \""..tostring(tr.HitPos).."\"\n")
+    local strvc = tostring(tr.HitPos)
+    Notify(ply, "Position: ["..strvc.."]", "GENERIC")
+    print("Position: "..strvc)
+    ply:ConCommand(gsTool.."_position \""..strvc.."\"\n")
   else
+    local up = Vector(0,0,1)
+    local dt = tr.HitNormal:Dot(up)
     tr.Entity:GetPhysicsObject():EnableMotion(false)
+    if(dt > 0.9) then
+      Notify(ply, "Angle mismatch for ["..dt.."]", "ERROR")
+      return false
+    end
     local angle = tr.HitNormal:AngleEx(Vector(0,0,1))
     local value = tr.Entity:WorldToLocalAngles(angle)
-    print("Custiom angle: " ..tostring(value))
-    ply:ConCommand(gsTool.."_angle \""..tostring(value).."\"\n")
+    local strvc = tostring(value)
+    print("Custiom angle: " ..strvc)
+    Notify(ply, "Custiom angle: ["..strvc.."]", "GENERIC")
+    ply:ConCommand(gsTool.."_angle \""..strvc.."\"\n")
   end
   return true
 end
